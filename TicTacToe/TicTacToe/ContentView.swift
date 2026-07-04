65e1360e2b303ce1cbde3eab31906392737b3c61
Andrew Bell <andrew@thatopsguy.com>
2026-07-04 09:37:39 +0100

    Created Xcode project

---
+++ TicTacToe/TicTacToe/ContentView.swift
@@ -0,0 +1,24 @@
+//
+//  ContentView.swift
+//  TicTacToe
+//
+//  Created by Andrew Bell on 04/07/2026.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+#Preview {
+    ContentView()
+}
---
+++ TicTacToe/TicTacToe/TicTacToeApp.swift
@@ -0,0 +1,17 @@
+//
+//  TicTacToeApp.swift
+//  TicTacToe
+//
+//  Created by Andrew Bell on 04/07/2026.
+//
+
+import SwiftUI
+
+@main
+struct TicTacToeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
